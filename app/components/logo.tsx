a841c6c945ebd638771ded5e5434fef878924207
taqiudeen275 <55208368+taqiudeen275@users.noreply.github.com>
2024-07-12 12:30:40 +0000

    login and register completed successfully

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-3 bg-background p-1 pl-8 text-xl rounded-full" >
+    <Link href="/" className="flex items-center gap-3 bg-background p-1 pl-8 text-xl rounded-full" >
     My
     <div className="bg-foreground p-1 text-xl text-background pl-4 pr-4 rounded-full">E Shop</div>
-   </div>
+   </Link>
   )
 }
 
