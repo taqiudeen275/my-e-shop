4023c829d8469aba36fe3101b3d9359f3e10a790
pious <109950629+pious2847@users.noreply.github.com>
2024-07-18 17:03:16 +0000

    Changes to be committed: 	modified:   app/components/card.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Hero from "./components/hero";
 export default async function Home() {
   return (
     <>
-    
       <Hero />
     <main className="flex min-h-screen flex-col items-center  p-24">
       
