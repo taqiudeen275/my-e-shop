0429b91e119845853cde8d0715cd6c134c22cca4
taqiudeen275 <55208368+taqiudeen275@users.noreply.github.com>
2024-09-07 19:50:30 +0000

    fixed cvolor issue and cart on progress

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,53 +1,37 @@
 'use client'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CartItem } from "./components";
-import { getCart, CartItemProp } from "@/lib/cart";
 import { Table, TableBody, TableHead, TableRow } from "@/components/ui/table";
+import router from "next/router";
+import { getCart, getProductById } from "../sever/general";
+import { useCookies } from 'next-client-cookies';
+import { AuthModel } from "pocketbase";
+import pb from "@/lib/pocketbase_client";
 
 export default function CartPage() {
-  const cartProductItems = getCart()
+  const [user, setUser] = useState<AuthModel|null>();
 
+  const cartProductItems = getCart()
+  const cookies = useCookies();
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      pb.client.authStore.loadFromCookie(cookies.get('pb_auth')?? "") 
+      setUser(pb.client.authStore.model);
+      const cartResponse = await getCart(["cart_item"], `user="${pb.client.authStore.model?.id}"`);
+      console.log("fgfdghf", cartResponse[0])
+
+    };
+
+    fetchInitialData();
+  }, [cookies]);
   return (
     <div className="w-full  pt-24  p-12 sm:p-24">
       <h2 className="text-xl sm:text-3xl font-bold mb-4">My Cart</h2>
       <div className="flex flex-col lg:flex-row gap-4 justify-center items-start w-full">
         <div className="w-full lg:w-2/3 carttable rounded-md py-6">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 overflow-x-auto">
-            <Table className="min-w-full divide-y divide-gray-200">
-              <thead >
-                <TableRow>
-                  <TableHead
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Product
-                  </TableHead>
-                  <TableHead
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Price
-                  </TableHead>
-                  <TableHead
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Quantity
-                  </TableHead>
-                  <TableHead
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Total Price
-                  </TableHead>
-                </TableRow>
-              </thead>
-              <TableBody className=" divide-y divide-gray-200">
-                {cartProductItems.map((product) => (
-                  <CartItem key={product.id} cartProduct={product} />
-                ))}
-              </TableBody>
-            </Table>
+           
           </div>
         </div>
         <div className="w-full lg:w-1/3 mb-5 px-4 md:px-8">
--- app/products/product-detail/[id]/details/page.tsx
+++ app/products/product-detail/[id]/details/page.tsx
@@ -2,19 +2,23 @@
 import React, { useEffect, useState } from "react";
 import { ProductImages, StarRating } from "./component";
 import { RecordModel } from "pocketbase";
-import { getProductById } from "@/app/sever/general";
+import { addToCart, createCartItem, getCart, getCartById, getProductById, updateCartById, updateCartItemById } from "@/app/sever/general";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Footer from "@/app/components/footer";
-import {ReviewComponent,ReviewsList} from "@/app/components/review";
+import { ReviewComponent, ReviewsList } from "@/app/components/review";
 import { ToastAction } from "@/components/ui/toast"
 import { useToast } from "@/components/ui/use-toast"
+import { useCookies } from "next-client-cookies";
+import pb from "@/lib/pocketbase_client";
 
 
 const ProductDetails = ({ params }: { params: { id: string } }) => {
   const [product, SetProduct] = useState<RecordModel | null>();
+  // varients and colors are the selected colors or varients 
   const [varient, setVarient] = useState<RecordModel | null>();
+  const [color, setColor] = useState<RecordModel | null>();
   const router = useRouter();
   const [quantity, setQuantity] = useState(1);
   const [currentColor, setCurrentColor] = useState("");
@@ -22,26 +26,40 @@ const ProductDetails = ({ params }: { params: { id: string } }) => {
   const [reviewTempUpdate, setReviewTempUpdate] = useState(0);
 
   const { toast } = useToast()
+  const cookies = useCookies();
 
   useEffect(() => {
+
+  
     const fetchInitialData = async () => {
       const productResponse = await getProductById(params.id, ["images", "colors", "varients"]);
-      console.log("fgfdghf", productResponse)
 
-      productResponse?.expand?.varients && setCurrentVarients(productResponse?.expand?.varients[0]?.id)
-      productResponse?.expand?.varients && setVarient(productResponse?.expand?.varients[0])
+      productResponse?.expand?.varients && setCurrentVarients(productResponse?.expand?.varients[0]?.id);
+      productResponse?.expand?.varients && setVarient(productResponse?.expand?.varients[0]);
+      if (productResponse?.colors.length > 0) {
+        setCurrentColor(productResponse?.colors[0]?.id);
+        setColor(productResponse?.colors[0]);
+      }
       SetProduct(productResponse);
     };
 
     fetchInitialData();
-  }, [params.id, router]);
+  }, [cookies, params.id, router]);
 
   const handleReviewSubmit = (reviewData: any) => {
-    setReviewTempUpdate(reviewTempUpdate+1)
+    setReviewTempUpdate(reviewTempUpdate + 1)
     toast({
       description: "Your review has been sent.",
     })
   };
+  const onAddToCart = async () => {
+
+    pb.client.authStore.loadFromCookie(cookies.get('pb_auth') ?? "")
+    const user = pb.client.authStore.model
+
+    await addToCart(user?.id, product!.id, quantity, product!.price, varient?.id, color?.id
+    );
+  }
 
   return (
     <>
@@ -62,36 +80,37 @@ const ProductDetails = ({ params }: { params: { id: string } }) => {
               <StarRating rating={product?.ratings ?? 0} /> {product?.ratings}
             </h2>
             <h1 className="text-xl sm:text-2xl ">
-              $ { product?.expand?.varients? varient?.price : (product?.price ?? "Product price")}
+              $ {product?.expand?.varients ? varient?.price : (product?.price ?? "Product price")}
             </h1>
 
-            <p className="sm:text-xl">
+            {(product?.expand?.varients ? varient?.stock === 0 : product?.stock === 0) ? <p className="sm:text-xl text-white px-4 py-1 bg-red-600/70 rounded-md mr-2 w-max">Out of Stock</p> : <p className="sm:text-xl">
               <span className="px-4 py-1 bg-primary/20 rounded-md mr-2">
-               { product?.expand?.varients? varient?.stock : (product?.stock ?? "")}
+                {product?.expand?.varients ? varient?.stock : (product?.stock ?? "")}
               </span>{" "}
               left in Stock
-            </p>
+            </p>}
             {product?.expand?.varients && <div className="space-y-3">
               <p className="text-xl">Varients</p>
               <div className="space-x-3 flex flex-wrap">
-                {product?.expand?.varients.map((v:any ) =>   <button key={v.id} className={`bg-primary/20 border-primary ${currentVarients === v.id ? 'border-2': ''}  px-8 py-2 rounded-full`} onClick={()=> {setCurrentVarients(v.id); setVarient(v); setQuantity(1)}}>
+                {product?.expand?.varients.map((v: any) => <button key={v.id} className={`bg-primary/20 border-primary ${currentVarients === v.id ? 'border-2 animate-bounce' : ''}  px-8 py-2 rounded-full`} onClick={() => { setCurrentVarients(v.id); setVarient(v); setQuantity(1) }}>
                   {v.name}
                 </button>)}
-              
+
               </div>
             </div>}
-            {product?.colors.length > 0  &&<div className="space-y-3">
+            {product?.colors.length > 0 && <div className="space-y-3">
               <p className="text-xl">Colours</p>
               <div className="space-x-3">
-                <button className="bg-red-600/20 px-4 py-2 rounded-full">
-                  R
-                </button>
-                <button className="bg-blue-600/20 border-blue-600 border-2  px-4 py-2 rounded-full">
+
+                {product?.colors.map((c: any) => <button key={c.id} className={`bg-${c.name.toLowerCase()}-600/20 px-4 py-2 rounded-full transition-all ${currentColor === c.id ? `border-2  border-${c.name.toLowerCase()}-600 animate-bounce` : ''} `} onClick={() => { setCurrentColor(c.id); setColor(c); }}>
+                  {c.name[0]}
+                </button>)}
+                {/* <button className="bg-blue-600/20 border-blue-600 border-2  px-4 py-2 rounded-full">
                   B
                 </button>
                 <button className="bg-green-600/20 px-4 py-2 rounded-full">
                   G
-                </button>
+                </button> */}
               </div>
             </div>}
             <div className="flex flex-wrap gap-4">
@@ -104,15 +123,14 @@ const ProductDetails = ({ params }: { params: { id: string } }) => {
                   placeholder="quantity"
                   type="number"
                   value={quantity}
-                  max={product?.expand?.varients? varient?.stock : product?.stock}
+                  max={product?.expand?.varients ? varient?.stock : product?.stock}
                   onChange={(e) => setQuantity(parseInt(e.target.value))}
                 />
               </div>
               {
-                (product?.expand?.varients? varient?.stock === 0 : product?.stock === 0)
-              ? <Button disabled>Add To Cart</Button> :  <Button>Add To Cart</Button>
+                (product?.expand?.varients ? varient?.stock === 0 : product?.stock === 0)
+                  ? <Button disabled>Add To Cart</Button> : <Button onClick={onAddToCart}>Add To Cart</Button>
               }
-              <Button variant={"outline"}>Buy Now</Button>
             </div>
           </div>
           <div className="col-span-5 lg:col-span-3">
@@ -127,9 +145,9 @@ const ProductDetails = ({ params }: { params: { id: string } }) => {
           />
         </div>
         <div className="mt-12">
-        <h1 className="text-xl sm:text-2xl mb-4">Reviews </h1>
+          <h1 className="text-xl sm:text-2xl mb-4">Reviews </h1>
 
-        <ReviewsList productId={product?.id??"null"} updated={reviewTempUpdate} />
+          <ReviewsList productId={product?.id ?? "null"} updated={reviewTempUpdate} />
         </div>
       </section>
       <Footer />
